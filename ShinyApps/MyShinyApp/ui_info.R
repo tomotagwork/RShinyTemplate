2621d53bcd218f3eb44b41d519d6e866c5f918f7
tomotagwork <tomotagwork@gmail.com>
2019-04-08 13:22:22 +0900

    fix typo

--- ShinyApps/MyShinyApp/ui_info.R
+++ ShinyApps/MyShinyApp/ui_info.R
@@ -1,7 +1,7 @@
 tabItem_info <- tabItem("tab_info",
                         div(p("[2019/03/26, ver0.1]"),align="right"),
                         div(img(src="bigorb.png", width="75px", height="75px"),align="center"),
-                        h1("MyShiinyAppテンプレート"),
+                        h1("MyShinyAppテンプレート"),
                         hr(),
                         span(p("一部Login処理が必要な操作があります。"), style="color:red"),
                         span(p("利用後は速やかに管理画面からサーバーを破棄して下さい。"), style="color:red"),
